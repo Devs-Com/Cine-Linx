c4db42e092a955d10ec0258d7cbab9391134b410
Anderson Mendoza <davidmramos53@gmail.com>
2023-05-18 21:32:54 -0500

    Initial commit

---
+++ src/main/java/com/services/cinelinx/CineLinxApplication.java
@@ -0,0 +1,13 @@
+package com.services.cinelinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineLinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineLinxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/services/cinelinx/CineLinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.cinelinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineLinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
