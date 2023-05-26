2c25f552ce125891f175418884dff6f70b37686b
Anderson Mendoza <davidmramos53@gmail.com>
2023-05-26 13:22:37 -0500

    agregando instancia SpringDataDialect

--- src/main/java/com/services/cinelinx/CineLinxApplication.java
+++ src/main/java/com/services/cinelinx/CineLinxApplication.java
@@ -2,6 +2,8 @@ package com.services.cinelinx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CineLinxApplication {
@@ -10,4 +12,8 @@ public class CineLinxApplication {
 		SpringApplication.run(CineLinxApplication.class, args);
 	}
 
+	@Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+	}
 }
