e907922d1856397213e6cd136381206d4753e799
72782330 <72782330@certus.edu.pe>
2023-06-11 09:43:21 -0500

    Actualizar ruta base del controlador ProductoController y refactorizar consulta de productos en el controlador

--- src/main/java/com/services/cinelinx/controller/ProductoController.java
+++ src/main/java/com/services/cinelinx/controller/ProductoController.java
@@ -1,20 +1,15 @@
 package com.services.cinelinx.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.services.cinelinx.model.Producto;
 import com.services.cinelinx.repository.ProductoRepository;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/productos")
 public class ProductoController {
 	
     @Autowired
@@ -23,8 +18,7 @@ public class ProductoController {
     @GetMapping("")
     ModelAndView index_confiteria() {
         List<Producto> Productos = productoRepository
-                .findAll(PageRequest.of(0, 4, Sort.by("precio").descending()))
-                .toList();
+                .findAll();
 
         return new ModelAndView("index_confiteria")
                 .addObject("Productos", Productos);
