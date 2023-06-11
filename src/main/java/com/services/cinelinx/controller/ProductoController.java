ab5245f88d56f45a835a9ee93849d484163a9255
72782330 <72782330@certus.edu.pe>
2023-06-11 12:04:20 -0500

    Corrección de la estructura

--- src/main/java/com/services/cinelinx/controller/ProductoController.java
+++ src/main/java/com/services/cinelinx/controller/ProductoController.java
@@ -1,12 +1,17 @@
 package com.services.cinelinx.controller;
-import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.services.cinelinx.model.Producto;
 import com.services.cinelinx.repository.ProductoRepository;
+
 
 @Controller
 @RequestMapping("/productos")
@@ -16,11 +21,11 @@ public class ProductoController {
     private ProductoRepository productoRepository;
 
     @GetMapping("")
-    ModelAndView productos() {
-        List<Producto> Productos = productoRepository
-                .findAll();
+    ModelAndView productos(@PageableDefault(sort = "precio", direction = Sort.Direction.DESC)
+    Pageable pageable) {
+        Page<Producto> productos = productoRepository.findAll(pageable);
 
         return new ModelAndView("productos")
-                .addObject("Productos", Productos);
+                .addObject("productos", productos);
     }
 }
