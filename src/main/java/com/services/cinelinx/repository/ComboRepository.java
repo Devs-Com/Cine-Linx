80f450e1840bee3beb82aadd79f1d81eef07b86c
72782330 <72782330@certus.edu.pe>
2023-07-12 11:49:19 -0500

    Renombrando repositorio a ComboRepository

---
+++ src/main/java/com/services/cinelinx/repository/ComboRepository.java
@@ -0,0 +1,7 @@
+package com.services.cinelinx.repository;
+
+import com.services.cinelinx.model.Combo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComboController extends JpaRepository<Combo, Integer> {
+}
--- src/main/java/com/services/cinelinx/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.services.cinelinx.repository;
-
-import com.services.cinelinx.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
