ad39054f4aaa735d5a444d223d85007bf9e366ba
72782330 <72782330@certus.edu.pe>
2023-07-12 11:43:33 -0500

    Creando modelo para Categoria

---
+++ src/main/java/com/services/cinelinx/model/Categoria.java
@@ -0,0 +1,20 @@
+package com.services.cinelinx.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table (name = "categorias")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nombre;
+}
