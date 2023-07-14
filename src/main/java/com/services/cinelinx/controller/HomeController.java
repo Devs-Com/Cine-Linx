90862832bab18b0c76e52de9a3bd378fa4587107
Anderson Mendoza <davidmramos53@gmail.com>
2023-07-13 19:37:16 -0500

    avance filtro por genero

--- src/main/java/com/services/cinelinx/controller/HomeController.java
+++ src/main/java/com/services/cinelinx/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.services.cinelinx.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.services.cinelinx.model.Genero;
@@ -47,10 +49,25 @@ public class HomeController {
         }
 
         @GetMapping("/peliculas")
-        ModelAndView listaPeliculas(
+        ModelAndView listaPeliculas(@RequestParam(value = "generoSeleccionado", required = false) Integer idGenero,
                         @PageableDefault(sort = "fechaEstreno", direction = Sort.Direction.DESC) Pageable pageable) {
-                Page<Pelicula> peliculas = peliculaRepository.findAll(pageable);
+                Page<Pelicula> peliculas;
                 List<Genero> generos = generoRepository.findAll(Sort.by("nombre"));
+
+                if (idGenero != null) {
+                        Optional<Genero> generoOptional = generoRepository.findById(idGenero);
+                        if (generoOptional.isPresent()) {
+                                Genero genero = generoOptional.get();
+                                peliculas = peliculaRepository.findByGeneroIdGenero(idGenero, pageable);
+                        } else {
+                                // Manejo de error si el género no existe
+                                // Por ejemplo, redirigir a una página de error
+                                return new ModelAndView("error");
+                        }
+                } else {
+                        peliculas = peliculaRepository.findAll(pageable);
+                }
+
                 return new ModelAndView("peliculas")
                                 .addObject("peliculas", peliculas)
                                 .addObject("generos", generos);
