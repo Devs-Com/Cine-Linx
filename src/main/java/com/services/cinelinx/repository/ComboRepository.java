9231677c0dd330010398db1b1737419ae98e23e9
Anderson Mendoza <davidmramos53@gmail.com>
2023-07-21 23:13:06 -0500

    eliminando todo acerca de combo

--- src/main/java/com/services/cinelinx/repository/ComboRepository.java
+++
@@ -1,7 +0,0 @@
-package com.services.cinelinx.repository;
-
-import com.services.cinelinx.model.Combo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComboRepository extends JpaRepository<Combo, Integer> {
-}
